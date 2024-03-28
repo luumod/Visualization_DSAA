4d6da6a313cfbcdd7d402af2160752490e1422f3
Yuleo <1173012900@qq.com>
2024-03-28 23:24:55 +0800

    Update bubblesort.cpp doxygen comments.

--- dsaa/Sort/BubbleSort.cpp
+++ dsaa/Sort/BubbleSort.cpp
@@ -13,8 +13,7 @@
 BubbleSort::BubbleSort(QObject *parent)
     : SortObject(parent)
 {
-    //属性动画控制交换动画效果
-    //animation.setDuration(2000);
+    // Initialize of the animation for bubble sort.
     animation.setStartValue(0.0);
     animation.setEndValue(1.0);
     animation.setEasingCurve(QEasingCurve::OutQuart);
@@ -23,18 +22,18 @@ BubbleSort::BubbleSort(QObject *parent)
     connect(&animation, &QVariantAnimation::valueChanged, this, &SortObject::updateRequest);
 }
 
-/*//一般的冒泡排序写法
-template<typename T>
-void bubble_sort(std::vector<T>& arr)
-{
-    for (size_t i = 0; i < arr.size() - 1; i++) {
-        for (size_t j = 0; j < arr.size() - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                std::swap(arr[j], arr[j + 1]);
-            }
-        }
-    }
-}*/
+// This is a template of the bubble sort.
+//template<typename T>
+//void bubble_sort(std::vector<T>& arr)
+//{
+//    for (size_t i = 0; i < arr.size() - 1; i++) {
+//        for (size_t j = 0; j < arr.size() - 1 - i; j++) {
+//            if (arr[j] > arr[j + 1]) {
+//                std::swap(arr[j], arr[j + 1]);
+//            }
+//        }
+//    }
+//}
 
 void BubbleSort::sort(int count, int interval)
 {
@@ -90,15 +89,16 @@ void BubbleSort::draw(QPainter *painter, int width, int height)
 {
     painter->setPen(QColor(200, 200, 200));
     const int len = arr.length();
-    //边框距离
+    
+    // Border distance.
     const int left_space = 0;
     const int right_space = 0;
     const int top_space = 20;
     const int bottom_space = 50;
 
-    const int item_space = 10; //柱子横项间隔
+    const int item_space = 10; //Column horizon space.
     const int text_height = painter->fontMetrics().height();
-    const int text_space = 15; //文字和柱子间隔
+    const int text_space = 15; // Text and column's space
     const double item_width = (width + item_space - left_space - right_space) / (double)len - item_space;
     const double item_bottom = height - bottom_space;
     const double height_factor = (height - top_space - bottom_space) / (double)len;
@@ -107,32 +107,33 @@ void BubbleSort::draw(QPainter *painter, int width, int height)
     QColor color;
     for (int i = 0; i < len; i++)
     {
-        //色块位置x
+        // Color block position x.
         item_left = left_space + i * (item_width + item_space);
         item_height = height_factor * arr.at(i);
-        //色块颜色
+        // The column's color.
         color = QColor(200, 200, 200);
-        //在执行排序操作的时候标记比较的两个元素
         if (getRunFlag()) {
             if (i == arrJ) {
+                // The first element.
                 color = QColor(255, 170 , 0);
                 if (swapFlag) {
                     item_left += animation.currentValue().toDouble() * (item_width + item_space);
                 }
             } else if (i == arrJ + 1) {
+                // The second element.
                 color = QColor(0, 170 , 255);
                 if (swapFlag) {
                     item_left -= animation.currentValue().toDouble() * (item_width + item_space);
                 }
             } else if (i >= len - arrI) {
-                //已排序好的
+                // Remaining elements.
                 color = QColor(0, 170, 0);
             }
         }
-        //画文字
+        // Draw Text
         painter->drawText(item_left, item_bottom + text_height + text_space,
                           QString::number(arr.at(i)));
-        //画色块柱子
+        // Draw column rect.
         painter->fillRect(item_left, item_bottom - item_height,
                           item_width, item_height,
                           color);
@@ -145,7 +146,6 @@ void BubbleSort::initArr(int count)
         return;
     }
 
-    //初始化并随机打乱数据
     arr.resize(count);
     for (int i = 0; i < count; i++)
     {
