a1ca90621308f3f2ce1a222880cf2b070cbda5e0
Yuleo <1173012900@qq.com>
2024-03-29 14:19:54 +0800

    update

---
+++ dsaa/Sort/MainCanvas.cpp
@@ -0,0 +1,73 @@
+﻿#include "MainCanvas.h"
+#include "SortObject.h"
+#include <QPaintEvent>
+#include <QPainter>
+#include <QThread>
+
+MainCanvas::MainCanvas(QWidget *parent)
+    : QWidget(parent)
+{
+    setAttribute(Qt::WA_StyledBackground, true);
+    thread = new QThread(this);
+}
+
+int MainCanvas::getSortType() const
+{
+    return sortType;
+}
+
+void MainCanvas::setSortObject(int type, SortObject *obj)
+{
+    if (sortType == type) {
+        return;
+    }
+    //删除旧的排序
+    if (sortObj) {
+        sortObj->deleteLater();
+        sortObj = nullptr;
+    }
+
+    sortType = type;
+    sortObj = obj;
+
+    if (sortObj) {
+        connect(sortObj, &SortObject::updateToDraw, this, [this]{
+            update();
+        });
+        connect(sortObj, &SortObject::runFlagChanged, this, &MainCanvas::runFlagChanged);
+    }
+    update();
+}
+
+void MainCanvas::sort(int count, int interval)
+{
+    if (sortObj) {
+        
+        sortObj->sort(count, interval);
+    }
+}
+
+void MainCanvas::stop()
+{
+    if (sortObj) {
+        sortObj->stop();
+    }
+}
+
+//void MainCanvas::intoThread() {
+//    if (sortObj) {
+//        sortObj->moveToThread(thread);
+//    }
+//}
+
+void MainCanvas::paintEvent(QPaintEvent *event)
+{
+    event->accept();
+
+    QPainter painter(this);
+    painter.fillRect(rect(),QColor(20,20,20));
+
+    if (sortObj) {
+        sortObj->draw(&painter, width(), height());
+    }
+}
