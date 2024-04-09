c4d04c901c4ed29109a00474421cc62d369f5380
Yuleo <1173012900@qq.com>
2024-04-09 18:18:22 +0800

    Test the logical of the linked list.

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <QApplication>
 #include <QWidget>
 #include <QPainter>
+#include <QPushButton>
 
 void drawLinkedList(QPainter* painter, const QList<int>& values, int width, int height) {
 	painter->setPen(QColor(200, 200, 200));
@@ -106,8 +107,31 @@ void drawLinkedList(QPainter* painter, const QList<int>& values, int width, int 
 class TestWidget : public QWidget {
 public:
 	TestWidget(const QList<int>& values) : m_values(values) {
+		this->setMinimumSize(300, 300);
+		init();
 	}
-
+	void init() {
+		addNode();
+		delNode();
+	}
+	void addNode() {
+		btn_add = new QPushButton("Add", this);
+		btn_add->move(10, 10);
+		connect(btn_add, &QPushButton::clicked, this, [=]() {
+			m_values.push_back(1);
+			update();
+		});
+	}
+	void delNode() {
+		btn_pop_back = new QPushButton("Del", this);
+		btn_pop_back->move(btn_add->pos().x() + btn_add->width(), 10);
+		connect(btn_pop_back, &QPushButton::clicked, this, [=]() {
+			if (!m_values.empty()) {
+				m_values.pop_back();
+				update();
+			}
+		});
+	}
 protected:
 	void paintEvent(QPaintEvent* event) override {
 		QPainter painter(this);
@@ -116,6 +140,8 @@ protected:
 
 private:
 	QList<int> m_values;
+	QPushButton* btn_add{ nullptr };
+	QPushButton* btn_pop_back{ nullptr };
 };
 
 int main(int argc, char* argv[]) {
@@ -123,8 +149,6 @@ int main(int argc, char* argv[]) {
 
 	// 创建链表数据
 	QList<int> values;
-	values << 1 << 2 << 3 << 4 << 5 
-		<< 11 ;
 
 	// 创建窗口并显示
 	TestWidget widget(values);
